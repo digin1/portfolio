45f138655e801d2d59576167c9677c006d7c4656
digin1 <digin13dominic@gmail.com>
2022-05-17 20:16:19 +0100

    migrated to digindominic.me

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ npm run build
 # navigate into the build output directory
 cd dist
 # if you are deploying to a custom domain
-echo 'demo.digindominic.me' > CNAME
+echo 'digindominic.me' > CNAME
 git init
 git add -A
 git commit -m 'deploy'
