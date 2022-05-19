3e22931cd96d681a978c08d1f58254d90a4b2663
digin1 <digin13dominic@gmail.com>
2022-05-20 00:21:53 +0100

    reformatted json object

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,8 @@
         <div class="border-top my-3"></div>
         <AboutMe :user="user"/>
         <div class="border-top my-3"></div>
-        
+        <EducationExperience/>
+        <div class="border-top my-3"></div>
         Portfolio created with <a href="https://github.com/digin1/portfolio"><b-icon-github></b-icon-github>Vue.js</a> | 2022 &copy; <a href="https://linkedin.com/in/digin">Digin Dominic</a>
   </div>
 </div>
@@ -18,15 +19,18 @@
 
 import HeaderPage from './components/HeaderPage.vue'
 import AboutMe from './components/AboutMe.vue'
+import EducationExperience from './components/EducationExperience.vue';
 
 import { bucket } from "./cosmic.js";
+
 
 export default {
   name: 'App',
   components: {
     AboutMe,
-    HeaderPage
-  },
+    HeaderPage,
+    EducationExperience,
+},
   data: () => ({
       loading: false,
       posts: null,
---
+++ src/components/EducationExperience.vue
@@ -0,0 +1,45 @@
+<template>
+
+
+    <h5><b>Experience</b></h5>
+    <div class="border-top my-3"></div>
+    <div class="container-fluid">
+        <div class="row p-2 justify-content-center">
+            <div class="col-sm-5 p-1">
+                <div class=" card text-start">
+                    <div class="card-header">
+                        <b>EDUCATION</b>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">An item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-5 p-1">
+                <div class="card text-start">
+                    <div class="card-header">
+                        <b>PROFESSIONAL</b>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">An item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</template>
+
+<script>
+export default {
+    name: 'EducationExperience',
+}
+</script>
+
+<style scoped>
+</style>
