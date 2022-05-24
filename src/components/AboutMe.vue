6842ea49d2cb4095d8cbc29b824ac5082b6ef4b1
digin1 <digin13dominic@gmail.com>
2022-05-24 17:13:13 +0100

    social icons added

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -13,11 +13,18 @@
       </div>
       
       <div class="col-md-4 card-mobile">
-        <div class="paragraph text-start">
+        <div class="contact paragraph text-start">
               <p><b>Email:</b> {{user[0].metadata.email}}</p>
               <p><b>Phone:</b> {{user[0].metadata.phone}}</p>
               <p><b>City:</b> {{user[0].metadata.city}}</p>
               <p><b>Languages:</b> {{user[0].metadata.languages}}</p>
+              <div class="social">
+                    <a :href="user[0].metadata.facebook"><b-icon-facebook class="m-1"></b-icon-facebook></a>
+                    <a :href="user[0].metadata.instagram"><b-icon-instagram class="m-1"></b-icon-instagram></a>
+                    <a :href="user[0].metadata.linkedin"><b-icon-linkedin class="m-1"></b-icon-linkedin></a>
+                    <a :href="user[0].metadata.github"><b-icon-github class="m-1"></b-icon-github></a>
+                    <a :href="user[0].metadata.twitter"><b-icon-twitter class="m-1"></b-icon-twitter></a>
+              </div>
         </div>
       </div>
     </div>
@@ -35,5 +42,18 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-
+.social{
+    font-size: 25px;
+}
+.contact.paragraph.text-start{
+     line-height:90%;
+}
+a{ 
+  color:inherit;
+}
+a:hover {
+  color: black;
+  padding-bottom:7px;
+  border-bottom:1px solid black;
+}
 </style>
